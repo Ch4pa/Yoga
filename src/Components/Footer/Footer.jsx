e5c1f0055dca6c44911c76dfb35ac54dfc365d21
Mateo Sabelli <mateosabelli4a@gmail.com>
2023-08-14 18:03:31 -0300

    problema vercel

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,14 +1,26 @@
 import React from "react";
-import logo from "../../../public/logo.png";
 import "./Footer.css";
+import { BsWhatsapp } from "react-icons/bs";
+import { BiLogoInstagram } from "react-icons/bi";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
     <div className="footerBox">
       <div className="logoImg">
-        <a href="#">
-          <img src={logo} className="imgLogo" alt="logo" title="Logo" />
-        </a>
+        <Link>
+        <BsWhatsapp />
+        </Link>
+        <Link
+          to="https://www.instagram.com/belu_brizzi_yoga/ "
+          target="_Blank"
+          className="navItem"
+        >
+          <BiLogoInstagram className="logoInstagram" />
+        </Link>
+      </div>
+      <div>
+        <p>@2023 Yogapalermo. Todos los derechos reservados</p>
       </div>
     </div>
   );
--- src/Components/SobreMi/SobreMi.jsx
+++ src/Components/SobreMi/SobreMi.jsx
@@ -3,7 +3,49 @@ import "./SobreMi.css";
 function SobreMi() {
   return (
     <div className="caja" id="sobre_mi">
-      SobreMi
+      <div className="boxObjetivo">
+        <p className="tituloObjetivo">Objetivo</p>
+        <p className="textoObjetivo">
+          Nuestro objetivo es lograr que encuentres un espacio en donde puedas
+          conectar con tu ser interior... Donde puedas alcanzar el equilibrio
+          entre la mente , el cuerpo y el espíritu... Un lugar en donde
+          relajarte y vivir el yoga en plenitud sea tu única meta. ..
+        </p>
+        <img src="../../public/profesora.jpeg" alt="" className="imgProfesora"/>
+      </div>
+      <div className="boxTrayectoria">
+        <p className="tituloTrayectoria">Mi trayectoria</p>
+        <p className="textoTrayectoria">
+          El yoga entró en mi vida hace 14 años, cuando comencé a practicar con
+          mi querida amiga Tiny! Quien supo contagiarme con esa vocación inmensa
+          que posee por esta disciplina. Amaba la práctica de yoga ( integral
+          pero mucho más amaba la practica de yoga artístico, ) era ahí donde
+          podía sentir que solo era yo, el mat y la música nada más importaba. Y
+          fue en ese momento en donde descubrí mi pasión por esta hermosa
+          disciplina y profundice aún más mí práctica. 
+          <br />
+          Fue un año después que decidí formarme como profesora de Yoga integral para luego continuar
+          con la formación de distintos estilos de yoga, como yoga coreográfico,
+          Yoga dinámico, yoga flow, vinyasa Yoga, Power Yoga, Yoga deportivo. Y
+          meditacion. 
+          <br />
+          Hoy en dia como hace 14 años atrás les puedo asegurar que
+          el yoga es mi pasión, mi verdadera vocación, seguir avanzando en este
+          camino, practicar cada día esta disciplina me llena el alma, pero lo
+          que más me apasiona es compartir mis enseñanzas con otras personas,
+          porq los beneficios del yoga son tantos y las herramientas para lograr
+          un bienestar general son sin dudas únicas. 
+          <br />
+          Realmente la práctica de
+          yoga cambió mi vida por completo y también quiero ayudarte a que
+          cambie la tuya. 
+          <br />
+          <br />
+          Te espero sobre el Mat!
+          <br /> 
+          Namaste 🙏
+        </p>
+      </div>
     </div>
   );
 }
