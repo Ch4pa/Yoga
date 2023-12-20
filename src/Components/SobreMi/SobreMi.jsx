fedac6a4b59e5b2cbfdac613c470c058f76a86bb
Mateo Sabelli <mateosabelli4a@gmail.com>
2023-12-20 11:31:31 -0300

    pagina nueva

--- src/Components/SobreMi/SobreMi.jsx
+++ src/Components/SobreMi/SobreMi.jsx
@@ -1,9 +1,46 @@
 import React from "react";
 import "./SobreMi.css";
+import CardIntegral from "../../../public/Card2.jpg";
+import CardFlow from "../../../public/Card2.jpg";
+import CardPower from "../../../public/Card2.jpg";
 function SobreMi() {
   return (
     <div className="cajaMain">
-      <div className="caja">
+      <div className="boxNaranja">
+        <p className="textoBoxNaranja">
+          Clases De Yoga Personalizadas de YOGA Flow, Yoga Power y Yoga
+          Deportivo.
+        </p>
+      </div>
+
+      <div className="clasesYoga">
+        <h1>
+          Clases de <span>Yoga</span>
+        </h1>
+        <p>Estos son los estilos de Yoga que brindamos en Yoga Palermo</p>
+
+        <div className="cards">
+          <div className="cardFlow">
+            <img src={CardFlow} alt="" className="imgFlow" />
+            <p>
+              Yoga <span>Flow</span>
+            </p>
+          </div>
+          <div className="cardPower">
+            <img src={CardIntegral} alt="" className="imgPower" />
+            <p>
+              Yoga <span>Power</span>
+            </p>
+          </div>
+          <div className="cardIntegral">
+            <img src={CardPower} alt="" className="imgIntegral" />
+            <p>
+              Yoga <span>Integral</span>
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* <div className="caja">
         <p className="titulo">Inicio</p>
         <h1 className="contenido">
           Sumérgete en el mundo del yoga con nuestras emocionantes clases.
@@ -55,7 +92,7 @@ function SobreMi() {
             </p>
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
